1396fa201fab0549a3d2ce8ebe00f7d84ca8792b
João Vitor Carneiro <ayahkoskaiba@gmail.com>
2025-04-08 17:28:56 -0300

    Add files via upload
    Exercício 01 - Contato

---
+++ ex01/Telefone.java
@@ -0,0 +1,15 @@
+package br.org.serratec.ex01;
+
+public class Telefone {
+
+    protected String numero;
+
+    public Telefone(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+}
---
+++ ex01/TesteContato.java
@@ -0,0 +1,15 @@
+package br.org.serratec.ex01;
+
+public class TesteContato {
+    public static void main(String[] args) {
+        Estado estado = new Estado("Rio de Janeiro", "RJ");
+        Cidade cidade = new Cidade("Petrópolis", estado);
+        Endereco endereco = new Endereco("Rua Fonseca Ramos", "Centro", "25648180", cidade);
+        Contato contato = new Contato("Roni", endereco);
+
+        contato.adicionarTelefone();
+        System.out.println(contato.toString());
+        contato.mostrarTelefones();
+        
+    }
+}
